4cbd2648f378045c5aadc6a42454078b23ec16ee
Alfredo <alfredcastaldi1992@gmail.com>
2023-12-18 18:43:13 +0100

    changed line

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("cambiamento al main");
+
+    System.out.println("adesso è linea 4, come la mettiamo? ");
     
   }
 }
