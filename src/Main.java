e4766e653f029af46764f3ea0f2cd7912cf46ce1
Daniel <adi251007@gmail.com>
2023-12-20 10:50:54 +0100

    Modified function name as requested by fabio

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
-	public static void hello(String name) {
+	public static void sayHello(String name) {
 		System.out.println(name);
 	}
 	
 	public static void main(String[] args) {
-		hello("Daniel")
+		sayHello("Daniel")
 	}
 }
