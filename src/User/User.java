bfe10d4eab83b2b52a4ae1d5caa7d04f4a331340
Alfredo <alfredcastaldi1992@gmail.com>
2024-01-08 17:09:33 +0100

    fantoccio user

---
+++ src/User/User.java
@@ -0,0 +1,41 @@
+package User;
+
+import java.util.ArrayList;
+
+public class User {
+
+    // inizializzazione id ed incremento
+    private int userId;
+    private static int staticUserId;
+
+    private String userName;
+    private String userSurname;
+
+    /* i contatti dell'utente andranno dentro un arraylist
+       il tipo di dato dovrà essere un oggetto contact
+       contenente email, telefono e indirizzo
+    */
+    //private ArrayList<UserContacts> userContacts= new ArrayList<UserContacts>();
+
+    // il nostro utente avrà come proprietà la mansione la data d'assunzione
+
+    private String workingTask;
+    private Date hiredDate;
+
+    // proprietà per documentarne la fine del rapporto lavorativo
+
+    private Boolean isFired = false;
+    private Date dismissDate;
+
+    /*
+      proprietà per determinare la prestazione lavorativa giorno per giorno
+      dovrà essere un oggetto che verrà istanziato giornalmente
+      Times sarà il nome della sub classe per adesso
+     */
+
+    private Times dayPerformance;
+
+
+
+
+}
