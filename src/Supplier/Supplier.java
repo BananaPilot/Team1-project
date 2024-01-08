c8a1c2e5402765c42bd0c42bd9a5efa0d073f5d7
bananapilota <fabiopetrosillo2@gmail.com>
2024-01-08 16:10:54 +0100

    added bear bone supplier class waiting for development so i can continue

--- src/Supplier/Supplier.java
+++ src/Supplier/Supplier.java
@@ -1,4 +1,60 @@
 package Supplier;
 
 public class Supplier {
+  private int counter = 1;
+  private int ID;
+  private String companyName;
+  private String address;
+  private int suppliedProductType;
+  private String VATNumber;
+
+  //TODO operations;
+  //TODO contacts;
+
+  public Supplier(String companyName, String address, int suppliedProductType, String VATNumber) {
+    this.companyName = companyName;
+    this.address = address;
+    this.suppliedProductType = suppliedProductType;
+    this.VATNumber = VATNumber;
+  }
+
+  public int getID() {
+    return ID;
+  }
+
+  public String getCompanyName() {
+    return companyName;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public int getSuppliedProductType() {
+    return suppliedProductType;
+  }
+
+  public String getVATNumber() {
+    return VATNumber;
+  }
+
+  public void setCompanyName(String companyName) {
+    this.companyName = companyName;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public void setSuppliedProductType(int suppliedProductType) {
+    this.suppliedProductType = suppliedProductType;
+  }
+
+  public void setVATNumber(String VATNumber) {
+    this.VATNumber = VATNumber;
+  }
+
+  private int incrementAndGet() {
+    return counter ++;
+  }
 }
