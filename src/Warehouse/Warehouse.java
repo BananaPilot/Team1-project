5b0451ab6911f2cf73f26539411d525781f4d82a
Alfredo <alfredcastaldi1992@gmail.com>
2024-01-08 16:42:44 +0100

    warehouse fantoccio per nomenclatura

--- src/Warehouse/Warehouse.java
+++ src/Warehouse/Warehouse.java
@@ -22,48 +22,4 @@ public class Warehouse {
     public String zone;
     public int zoneId;
 
-    /* l'idea è salvare ogni istanza di posizione in una lista
-       per poi essere in gradi di poter attingere all'id della posizione
-       tramite l'id deal prodotto
-     */
-
-    // un metodo per istanziare una posizione ed assegnargli un prodotto
-    public static void productIsStocked(String nome, String brand, double price){
-        ProdottoFantoccio prodotto = new ProdottoFantoccio(nome,brand,price);
-        StockPosition newPosition = new StockPosition(prodotto);
-        positions.add(newPosition);
-        prodotto.posizione = positions.get(prodotto.id-1).getBatchId();
-
-    }
-    // un metodo per risalire all'id della posizione tramite id prodotto
-
-    public static int getPositionIdFromProduct(int productId){
-        return positions.get(productId-1).getBatchId();
-    }
-
-    //risalire al prodotto tramite posizione
-    public static ProdottoFantoccio getProductFromPosition(int position){
-        return positions.get(position-1).getProductInPosition();
-    }
-
-    //controllo tutte posizioni
-    public static void checkPositions(){
-        for (int i=0; i< positions.size(); i++){
-            System.out.println(positions.get(i));
-        }
-    }
-
-    // metodo per trovare tutte le posizioni in base al nome
-    public static ArrayList<Integer> checkProductByName(String name){
-        ProdottoFantoccio risultato;
-        ArrayList<Integer> myPositions = new ArrayList<>();
-        for (int i=0; i< positions.size(); i++){
-            if (positions.get(i).getProductInPosition().nome.equalsIgnoreCase(name)){
-                risultato =  positions.get(i).getProductInPosition();
-                myPositions.add(risultato.posizione);
-            }
-        }
-        return myPositions;
-    }
-
 }
