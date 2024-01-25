20d49f839dfe0c3c4ac140c2f4afe6350d8093f9
bananapilota <fabiopetrosillo2@gmail.com>
2024-01-25 10:39:56 +0100

    added main and read prompt

---
+++ src/prompts/customer/CustomerPrompts.java
@@ -0,0 +1,19 @@
+package prompts.customer;
+
+public class CustomerPrompts {
+  public static void customerMainPrompt() {
+    System.out.println("1. Create a new customer");
+    System.out.println("2. List all customers");
+    System.out.println("3. List a specific customer");
+    System.out.println("4. Update a customer");
+    System.out.println("5. Delete a customer");
+    System.out.println("0. Back to main menu");
+  }
+
+  public static void customerSearchPrompt() {
+    System.out.println("1. Search by ID");
+    System.out.println("2. Search by E-mail");
+    System.out.println("3. Search by name and surname");
+    System.out.println("0. Back to previous menu");
+  }
+}
