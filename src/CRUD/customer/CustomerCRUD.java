f241148dfef2c665aae55d4638b276d7de1a5827
bananapilota <fabiopetrosillo2@gmail.com>
2024-01-25 11:47:01 +0100

    finished customer update

--- src/CRUD/customer/CustomerCRUD.java
+++ src/CRUD/customer/CustomerCRUD.java
@@ -27,4 +27,25 @@ public class CustomerCRUD {
       default -> null;
     };
   }
+
+  public static void updateCustomer() {
+    Customer customer = getCustomer();
+    if (customer == null){
+      System.out.println("Something went wrong please try again");
+      return;
+    }
+    int input;
+    do {
+      CustomerPrompts.customerUpdatePrompt();
+      input = Input.getInt();
+      switch (input) {
+        case 1 -> customer.setName(Input.getString("New name: "));
+        case 2 -> customer.setSurname(Input.getString("New Surname: "));
+        case 3 -> customer.setAddress(Input.getString("New address: "));
+        case 4 -> customer.getContacts().setEmail(Input.getString("New E-mail: "));
+        case 5 -> customer.getContacts().setEmail(Input.getString("New Phone-number: "));
+      }
+    } while (input != 0);
+    System.out.println("Updated Customer: " + customer);
+  }
 }
