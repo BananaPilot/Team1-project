cbce7bee6c93235ce3c1e7dcf1100fee35fc9550
bananapilota <fabiopetrosillo2@gmail.com>
2024-01-30 14:14:11 +0100

    main cleard

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
-import classes.customer.Customer;
+
 import classes.in.Input;
 import interactions.customer.CustomerInteractions;
 
-import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
-  public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-    CustomerInteractions.CustomerHandler();
+  public static void main(String[] args) {
     Input.getScanner().close();
   }
 }
