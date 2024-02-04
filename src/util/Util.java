1eb58ed67a659f2dee836d2bb2988bb0e3f907bf
bananapilota <fabiopetrosillo2@gmail.com>
2024-02-04 12:25:12 +0100

    removed javaDOC

--- src/util/Util.java
+++ src/util/Util.java
@@ -17,15 +17,6 @@ public class Util {
     return first.toUpperCase() + rest;
   }
 
-  /**
-   *
-   * Let's the user decide which element to select from an array list and returns it
-   *
-   * @param description description of the user input
-   *
-   * @return an @element of the array list taken in input based on the @index specified by the user
-   * @since 0.1
-   */
   public static Object select(ArrayList<?> genericCollection, String description){
     System.out.print(description);
     for(int i = 0; i < genericCollection.size(); i++) {
