587c6e2ef6371d8370a4ea477fd1a3a87d77817a
BananaPilot <fabiopetrosillo2@gmail.com>
2024-02-09 11:51:27 +0100

    fixed a bug where searchable was not working as expectes

--- src/classes/interfaces/Searchable.java
+++ src/classes/interfaces/Searchable.java
@@ -8,29 +8,21 @@ import java.util.ArrayList;
 public interface Searchable {
   default boolean contains(Object... values) {
     Field[] fields = this.getClass().getDeclaredFields();
-    for (Field field: fields){
-      if (field == null) continue;
-      return !checkAndPopulate(getMethodValue(Util.capitalize(field.getName())), values).isEmpty();
-    }
-    return false;
-  }
-
-  default Object getMethodValue(String filedName) {
-    try {
-      return this.getClass().getMethod("get" + filedName).invoke(this);
-    } catch (Exception e){
-      e.printStackTrace();
-    }
-    return null;
-  }
-
-  default ArrayList<Object> checkAndPopulate(Object res, Object... values) {
     ArrayList<Object> objects = new ArrayList<>();
-    for (Object value: values){
-      if (res == null) continue;
-      if (res.equals(value)) if (!objects.contains(this)) objects.add(this);
+    for (Field field : fields) {
+      if (field == null) continue;
+      String capField = Util.capitalize(field.getName());
+      for (Object value : values) {
+        try {
+          Object object = this.getClass().getMethod("get" + capField).invoke(this);
+          if (object == null) continue;
+          if (object.equals(value)) if (!objects.contains(this)) objects.add(this);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
     }
-    return objects;
+    return !objects.isEmpty();
   }
 
   static Searchable search(ArrayList<? extends Searchable> arrayList, Object... values){
