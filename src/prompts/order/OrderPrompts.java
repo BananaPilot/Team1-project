2da6d0356b767794428de1f4d453deb5e7dbec71
BananaPilot <fabiopetrosillo2@gmail.com>
2024-02-13 14:31:42 +0100

    finished crud now doing interactions

--- src/prompts/order/OrderPrompts.java
+++ src/prompts/order/OrderPrompts.java
@@ -1,7 +1,7 @@
 package prompts.order;
 
 public class OrderPrompts {
-    public static void SupplierOrderMainPrompt() {
+    public static void orderMainPrompt() {
         System.out.println("1. Create a new supplier order");
         System.out.println("2. List all orders of a supplier");
         System.out.println("3. List a specific order of a supplier");
@@ -10,9 +10,21 @@ public class OrderPrompts {
         System.out.println("0. Back to main menu");
     }
 
-    public static void SupplierOrderCreationPrompt() {
+    public static void orderCreationPrompt() {
         System.out.println("1. Select a product and add it to the order");
         System.out.println("2. Create a new product and add it to the order");
         System.out.println("0. to confirm the creation");
+    }
+
+    public static void orderSearchPrompt() {
+        System.out.println("1. Search by ID");
+        System.out.println("2. Search by date");
+        System.out.println("0. Back to main menu");
+    }
+
+    public static void orderUpdatePrompt() {
+        System.out.println("1. Update quantity");
+        System.out.println("2. Update product");
+        System.out.println("0. Back to previous menu");
     }
 }
