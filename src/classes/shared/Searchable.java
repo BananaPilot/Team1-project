78812ccc32d949d521c3aa7cb7bebd19d522ce0f
BananaPilot <fabiopetrosillo2@gmail.com>
2024-02-22 12:39:32 +0100

    Revert "removed fields and just got methods that we need"
    This reverts commit bd2ab9134dcb3a41fb7477efc0d859265e20e41a.

--- src/classes/shared/Searchable.java
+++ src/classes/shared/Searchable.java
@@ -1,34 +1,60 @@
 package classes.shared;
 
+import util.Util;
+
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public abstract class Searchable {
     public boolean contains(Object... values) {
         ArrayList<Object> objects = new ArrayList<>();
         for (Object value : values) {
-            for (Method method : getAllMethods(this.getClass())) {
+            for (Method method : getAllMethods(getAllFields(this.getClass()))) {
                 try {
                     Object object = method.invoke(this);
                     if (object == null) continue;
-                    if (object.equals(value) && !objects.contains(this)) objects.add(this);
+                    if (object.equals(value)) if (!objects.contains(this)) objects.add(this);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
+
         }
         return !objects.isEmpty();
     }
 
-    public List<Method> getAllMethods(Class<? extends Searchable> myClass) {
-        List<Method> methods = Arrays.asList(myClass.getDeclaredMethods());
-        methods.removeIf(method -> method.getName().contains("set") || method.getName().contains("to"));
+    public ArrayList<Method> getAllMethods(List<String> fields) {
+        ArrayList<Method> methods = new ArrayList<>();
+        for (String string : fields) {
+            if (string == null) continue;
+            try {
+                methods.add(this.getClass().getMethod("get" + Util.capitalize(string)));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         return methods;
     }
 
-    public static <T extends Searchable> T search(List<T> arrayList, Object... values) {
+    public List<String> getAllFields(Class<? extends Searchable> myClass) {
+        ArrayList<String> strings = new ArrayList<>();
+        Field[] fields = myClass.getDeclaredFields();
+        if (myClass.getSuperclass() != null) {
+            Field[] fields1 = myClass.getSuperclass().getDeclaredFields();
+            for (Field field1 : fields1) {
+                strings.add(field1.getName());
+            }
+        }
+        for (Field field : fields) {
+            strings.add(field.getName());
+        }
+        return strings;
+    }
+
+
+    public static <T extends Searchable> T search(ArrayList<T> arrayList, Object... values) {
         for (T searchable : arrayList) {
             if (searchable.contains(values)) return searchable;
         }
